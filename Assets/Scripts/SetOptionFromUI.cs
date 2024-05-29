3965ead2d4b0bbfb17d15b8a729c54265cfdfaf0
Raihana Zahra <rzahra@lion.lmu.edu>
2024-05-28 18:20:58 -0700

    updated file for scrollbar

--- Assets/Scripts/SetOptionFromUI.cs
+++ Assets/Scripts/SetOptionFromUI.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class SetOptionFromUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Scrollbar volumeSlider;
+    public TMPro.TMP_Dropdown turnDropdown;
+    public SetTurnTypeFromPlayerPref turnTypeFromPlayerPref;
+
+    private void Start()
     {
-        
+        volumeSlider.onValueChanged.AddListener(SetGlobalVolume);
+        turnDropdown.onValueChanged.AddListener(SetTurnPlayerPref);
+
+        if (PlayerPrefs.HasKey("turn"))
+            turnDropdown.SetValueWithoutNotify(PlayerPrefs.GetInt("turn"));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetGlobalVolume(float value)
     {
-        
+        AudioListener.volume = value;
+    }
+
+    public void SetTurnPlayerPref(int value)
+    {
+        PlayerPrefs.SetInt("turn", value); 
+        turnTypeFromPlayerPref.ApplyPlayerPref();
     }
 }
