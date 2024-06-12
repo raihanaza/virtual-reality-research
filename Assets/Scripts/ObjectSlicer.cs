289aee58a75d2dc48c98078670b12ac967009faa
Raihana Zahra <rzahra@lion.lmu.edu>
2024-06-12 16:10:56 -0700

    katana detects cube to slice,, added a start and end detection for katana, imported katana asset

---
+++ Assets/Scripts/ObjectSlicer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSlicer : MonoBehaviour
+{
+    public Transform startSlicingPoint;
+    public Transform endSlicingPoint;
+    public LayerMask sliceableLayer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 slicingDirection = endSlicingPoint.position - startSlicingPoint.position;
+        bool hasHit = Physics.Raycast(startSlicingPoint.position, slicingDirection, out hit, slicingDirection.magnitude, sliceableLayer);
+
+        if (hasHit)
+        {
+            Slice();
+        }
+    }
+
+    void Slice()
+    {
+        Debug.Log("object sliced!!!!");
+    }
+}
+
+
